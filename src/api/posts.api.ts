1c6c0f188865849ab6e786d1022489411fe74b12
markosbahgat <markos.bahgat@gmail.com>
2022-02-19 11:24:58 +0200

    First Commit

--- src/api/posts.api.ts
+++ src/api/posts.api.ts
@@ -5,7 +5,7 @@ import { INews } from '../models/interfaces/news.interface';
 
 export const GetPostsApi = async (PostOptions?: IPostOptions) => {
     try{
-        const response = await axios.get<INews>("https://newsapi.org/v2/everything?q=tesla&from=2022-02-16&to=2022-02-16&sortBy=popularity&apiKey=0a4f6e369bea4943a0fa39dae1b68af7")
+        const response = await axios.get<INews>(`https://newsapi.org/v2/everything?q=tesla&from=2022-02-16&to=2022-02-16&sortBy=popularity&apiKey=${process.env.REACT_APP_API_KEY}`)
         const data = response.data;
         return data;
     }
