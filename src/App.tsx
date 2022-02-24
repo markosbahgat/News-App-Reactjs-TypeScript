e6a5818fdbdf29cd2aefa7b86c45346ddcb654ff
markosbahgat <markos.bahgat@gmail.com>
2022-02-24 18:59:04 +0200

    Finishes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import './App.css';
 import '/node_modules/@fortawesome/fontawesome-free/css/all.css';
 import ImageSlider from './components/ImageSlider/ImageSlider';
-import { SectionTwo } from './HOC/newsPostsSection.hoc';
 import SlidingCard from './components/PostsSlider/SlidingCard';
 import FixedBottomFooter from './components/footer/FixedBottomFooter';
-import { PopularPostSection } from './HOC/popularPostsSection.hoc';
 import { MiddelSection } from './HOC/middelSection.hoc';
 
 function App() {
